087ca1a6c15d07cae2385bd295f14e23db1dbe20
CodesWithRobi <rayercodes@gmail.com>
2024-02-11 21:46:56 +0530

    added a client file

---
+++ echo-client.py
@@ -0,0 +1,14 @@
+import socket
+
+HOST = 'localhost'
+PORT = 9999
+SIZE = 1024
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+   s.connect((HOST, PORT))
+   msg = input("Enter a message to send")
+   s.sendall(byte(msg))
+   data = s.recv(SIZE) #Receiving from the server
+
+print(f"Received {data!r}")
+
