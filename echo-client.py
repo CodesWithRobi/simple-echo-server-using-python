f64ff6b932528bc4ed2c173fb16751803c66ccba
CodesWithRobi <rayercodes@gmail.com>
2024-02-11 21:53:45 +0530

    Fixed some bugs

--- echo-client.py
+++ echo-client.py
@@ -6,9 +6,9 @@ SIZE = 1024
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
    s.connect((HOST, PORT))
-   msg = input("Enter a message to send")
-   s.sendall(byte(msg))
+   msg = input("Enter a message to send : ")
+   s.sendall(bytes(msg, 'utf-8'))
    data = s.recv(SIZE) #Receiving from the server
 
-print(f"Received {data!r}")
+print(f"Received {data.decode()}")
 
